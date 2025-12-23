4f1c69058f241a9ff2cbce98e36c161a00f7f186
Deepa-0804 <deepacse51@gmail.com>
2025-12-23 23:02:48 +0530

    change

--- src/layouts/subComponents/MainMobileMenu.tsx
+++ src/layouts/subComponents/MainMobileMenu.tsx
@@ -1,6 +1,5 @@
 import mobileMenuData from '@/data/header-menu/mobileMenuData';
 import React from 'react';
-import Image from 'next/image';
 import Link from 'next/link';
 
 const MainMobileMenu = () => {
