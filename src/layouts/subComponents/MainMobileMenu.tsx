809abf9d8b08d3a40a9f0ee1d932e72262f27928
Deepa-0804 <deepacse51@gmail.com>
2025-12-23 23:01:18 +0530

    change

--- src/layouts/subComponents/MainMobileMenu.tsx
+++ src/layouts/subComponents/MainMobileMenu.tsx
@@ -1,5 +1,4 @@
 import mobileMenuData from '@/data/header-menu/mobileMenuData';
-import logo from "../../../public/assets/img/logo/logo.png";
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -7,11 +6,6 @@ import Link from 'next/link';
 const MainMobileMenu = () => {
     return (
         <>
-            <div className="tp-mobile-menu-logo">
-                <Link href="/">
-                    <Image width={120} src={logo} alt="logo" />
-                </Link>
-            </div>
             <ul>
                 {mobileMenuData.map((menuItem) => (
                     <li key={menuItem.id}>
